a681f6d9b0bfd904c98f6172b3cbb8503e8ebf24
SonicSyntax <156604117+SonicSyntax@users.noreply.github.com>
2024-01-21 16:22:18 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,52 +1,42 @@
-let lines = 5;
-const heights = [];
-const colors = ["#ff6f61","#4a90e2"," #ffd700","#8e44ad","#00b894","#3498db","#e74c3c","#2ecc71","#f39c12","#9b59b6","#1abc9c"]
+let loader = document.getElementById("loader");
+const lines = 5;
 
+const colors = ["#ff6f61","#4a90e2"," #ffd700","#8e44ad","#00b894"];
 
-function createArray(){
-    for(let i = 1;i <= 100;i++){
-        heights.push(i);
-    }
-}
-
-createArray();
-
-window.addEventListener("load", function () {
-    const loader = this.document.getElementById("loader");
-    let loaderStyle = this.getComputedStyle(loader);
-
-    let lineSize = (parseFloat(loaderStyle.width) / lines) + "px";
+window.addEventListener("load",function(){
+    let lineWidth = (parseFloat(getComputedStyle(loader).width) / lines) + "px";
 
     for(let i = 0;i < lines;i++){
-        let newLine = this.document.createElement("div");
+        let line = document.createElement("div");
+        line.id = `line${i}`;
 
-        newLine.id = `line${i}`;
+        line.style.height = "5px";
+        line.style.width = lineWidth;
+        line.style.transition = "0.5s";
 
-        newLine.style.width = lineSize;
-        newLine.style.height = "50px";
-        newLine.style.background = colors[i];
-        newLine.style.transition = "0.5s";
+        if(i in colors){
+            line.style.background = colors[i];
+        } else{
+            line.style.background = "black";
+        }
 
-        loader.appendChild(newLine);
+        loader.appendChild(line);
+
     }
 
-    function animate1(){
-        for (let i = 0; i < lines; i++) {
-            setTimeout(function () {
+    function animate(){
+        for(let i = 0;i < lines;i++){
+            setTimeout(function(){
                 let line = document.getElementById(`line${i}`);
-                let height = heights[i];
-        
-                line.style.height = (height + 50) + "px";
-        
+                line.style.height =  "5px";
                 setTimeout(function(){
-                    line.style.height = height + "px";
-                }, 500);
-            }, 100 * i);
+                    line.style.height = "50px";
+                },500);
+            },100 * i);
         }
     }
 
     this.setInterval(function(){
-        animate1();
-    },800);
-    
-});
+        animate();
+    },650);
+})
