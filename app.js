af8c32199b9959576286d87d99f586898c31c970
SonicSyntax <156604117+SonicSyntax@users.noreply.github.com>
2024-09-08 15:23:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const sidebar = document.getElementById("sidebar");
+const button1 = document.getElementById("button1");
 
-const userData={
-    currentSection:null,
-    description: null,
-};
-
-const updateUserData=(sectionId,description)=>{
-    userData.currentSection=sectionId;
-    userData.description=description;
+const userData = {
+    currentSection: "Kintamieji",
 }
 
-const btn1 = document.querySelector('.button1');
-btn1.addEventListener("click",()=>{
-    updateUserData("Kintamieji",["Pavizdys #1","Pavizdys #2","Išraiškos"]);
-    sidebar.innerHTML = "<header>Šioje skiltyje</header>";
-    for(obj of userData.description){
-        const HTML= `<span style="display:block;text-align:center;margin-top:5px;border-left:5px solid gray;background:rgba(255,165,0,0.5);font-weight:800"><a href="#${obj}" style="text-decoration:none;color:black;" href="#">${obj}</a></span>`;
-        sidebar.innerHTML+=HTML;
+const sections = [
+    {
+        id: "Kintamieji",
+        button_text: ["Pavizdys #1","Pavizdys #2"],
+        button_href: ["pavizdys1","pavizdys2"],
     }
-})
+];
+
+const updateSidebar = (section) => {
+    userData.currentSection = section.id;
+    sidebar.innerHTML = "<header>Šioje skiltyje</header>";
+
+    const buttonCount = section.button_text.length;
+    for(let i=0;i<buttonCount;i++){
+        sidebar.innerHTML += `<button><a href="#${section.button_href[i]}">${section.button_text[i]}</a></button>`;
+    }
+}
+
+window.addEventListener("load",updateSidebar(sections[0]));
+button1.onclick=updateSidebar(sections[0]);
